e3efd1ec9abe044f4c00c6424ab1223ce76479d4
RushiMThakker <rushimthakker@gmail.com>
2021-02-07 02:07:08 +0530

    Day6 Completed

--- flutter_catalog/lib/pages/login.dart
+++ flutter_catalog/lib/pages/login.dart
@@ -2,7 +2,15 @@ import "package:flutter/material.dart";
 import 'package:flutter/widgets.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changedButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -16,7 +24,7 @@ class LoginPage extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          Text("Welcome",
+          Text("Welcome $name",
               style: TextStyle(
                   color: Colors.purple,
                   fontSize: 30,
@@ -25,12 +33,16 @@ class LoginPage extends StatelessWidget {
             height: 20,
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
             child: Column(children: [
               TextFormField(
-                decoration: InputDecoration(
-                    hintText: "Enter username", labelText: "Username"),
-              ),
+                  decoration: InputDecoration(
+                      hintText: "Enter username", labelText: "Username"),
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {});
+                  }),
               TextFormField(
                   obscureText: true,
                   decoration: InputDecoration(
@@ -38,6 +50,32 @@ class LoginPage extends StatelessWidget {
               SizedBox(
                 height: 40,
               ),
+              InkWell(
+                onTap: () async {
+                  setState(() {
+                    changedButton = true;
+                  });
+                  await Future.delayed(Duration(seconds: 1));
+                  Navigator.pushNamed(context, MyRoutes.HOME_ROUTE);
+                },
+                child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    child: changedButton
+                        ? Icon(Icons.done, color: Colors.white)
+                        : Text("Login",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18)),
+                    width: changedButton ? 50 : 150,
+                    height: 50,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius:
+                            BorderRadius.circular(changedButton ? 50 : 8))),
+              )
+              /*
               ElevatedButton(
                   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
                   onPressed: () {
@@ -46,7 +84,7 @@ class LoginPage extends StatelessWidget {
                   child: Text(
                     "Login",
                     style: TextStyle(color: Colors.white),
-                  ))
+                  ))*/
             ]),
           )
         ]),
