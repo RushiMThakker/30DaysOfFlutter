04cba8d3dbf506493d5c38aa722df9766fa92123
RushiMThakker <rushimthakker@gmail.com>
2021-02-11 01:14:07 +0530

    Day10 completed

---
+++ flutter_catalog/lib/models/catalog.dart
@@ -0,0 +1,10 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final String color;
+  final String image;
+  final num price;
+
+  Item({this.id, this.name, this.desc, this.color, this.image, this.price});
+}
