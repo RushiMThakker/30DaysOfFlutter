a9dc2b248653c787296123f05b8ec0bc4e6d46ae
RushiMThakker <rushimthakker@gmail.com>
2021-02-12 02:45:12 +0530

    Day11 Completed

--- flutter_catalog/lib/pages/home.dart
+++ flutter_catalog/lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 
+// Day 11 we learnt about BuildContext, constraints and Widget tree, element tree and render tree
 class HomePage extends StatelessWidget {
   final int days = 50;
   final String name = "Rushi";
